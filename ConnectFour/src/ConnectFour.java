5b2978cfb942917be4e101b87478f7667ece9ede
diser <diser@asphaltpanthers.com>
2019-04-01 22:32:56 -0400

    Add main class for connect four.

---
+++ ConnectFour/src/ConnectFour.java
@@ -0,0 +1,27 @@
+
+public class ConnectFour {
+	public static void main(String[] args) {
+		Boolean validArgs = checkArgs(args);
+		
+		if (validArgs) {
+			int xSize = Integer.parseInt(args[0]);
+		}
+		else {
+			showUsageAndExit();
+		}
+	}
+	
+	private static Boolean checkArgs(String[] args) {
+		if (args.length == 1 && Integer.parseInt(args[0]) > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static void showUsageAndExit() {
+		System.out.println("Usage:");
+		System.out.println("java ConnectFour *x*");
+		System.out.println("*x* should be a nonnegative integer");
+		System.exit(1);
+	}
+}
